ff44637128b225dd04bf97f7c9b3506f1f397e63
David Newman <48318621+DavidNewmanGMIT@users.noreply.github.com>
2020-04-24 22:03:47 +0100

    comment about project

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//Geolocation imported
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+lon:number;
+lat:number;
+quiz:number;
 
-  constructor() {}
+  //Geolocation add to constructor, using lowercase to be clear
+  constructor(private geolocation: Geolocation) {
+
+  }
+
+  //Method for geolocation that uses the geolocation from 
+  geoMethod() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.lat = resp.coords.latitude;
+      this.lon = resp.coords.longitude;
+      this.quiz = this.lat + this.lon;
+      //lat and lon added together to get a number for the quiz, might need to
+      //make the equation more complex depended on how longitude and latitude works.
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
 
 }
